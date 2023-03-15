184922063b601c883350dcc746ca0a9aa3c1673e
sehyeon <hyeon4687@naver.com>
2023-03-15 20:07:54 +0900

    데이터 퍼블릭으로 옮김

--- src/App.tsx
+++ src/App.tsx
@@ -16,9 +16,7 @@ function App() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(
-          "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/19736c70-c41c-4a4a-b45f-b866eebab626/mock_data_example-flexsys.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230314%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230314T070629Z&X-Amz-Expires=86400&X-Amz-Signature=589af8f226b2aa5aaf7b5fe2a547a51f9c25696ad219c4bfe9b7b446fdfde563&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22mock_data_example-flexsys.json%22&x-id=GetObject"
-        );
+        const response = await axios.get("/data/chart_data.json");
         setData(response.data.response);
       } catch (err) {
         console.error(err);
